44c9dd1d277dc40cd50f21d0cc4e8413df483e60
Nikki A <nikki.apostolakis@gmail.com>
2019-01-26 18:40:43 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -9,6 +9,10 @@ class CouponsController < ApplicationController
   end
 
   def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
 
     redirect_to coupons_path
   end
