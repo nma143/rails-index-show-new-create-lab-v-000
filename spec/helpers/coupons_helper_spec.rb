3e3d8c6d4ecfdd2d87fdb95c90590c4559b39e94
Nikki A <nikki.apostolakis@gmail.com>
2019-01-26 18:57:59 +0000

    Automatically backed up by Learn

--- spec/helpers/coupons_helper_spec.rb
+++ spec/helpers/coupons_helper_spec.rb
@@ -12,4 +12,11 @@ require 'rails_helper'
 # end
 RSpec.describe CouponsHelper, type: :helper do
    #pending "add some examples to (or delete) #{__FILE__}"
+   describe CouponsHelper do
+     describe "string concat" do
+       it "concats two strings with spaces" do
+         expect(helper.concat_strings("this","that")).to eq("this that")
+       end
+     end
+   end
 end
